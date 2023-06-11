f5d2f72601cc633a5db2a42dd6c6e73148c4e2d8
anandp2002 <84928066+anandp2002@users.noreply.github.com>
2023-06-11 22:36:16 +0530

    Add files via upload

---
+++ leaky.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main()
+{
+ int incoming,outgoing,buck_size,n,store=0;
+ printf("Enter bucket size,outgoing rate and no: of inputs: ");
+ scanf("%d %d %d",&buck_size,&outgoing,&n);
+ while(n!=0)
+ {
+  printf("Enter the incoming pact size: ");
+  scanf("%d",&incoming);
+  printf("Incoming packe size %d\n",incoming);
+  if(incoming<=(buck_size-store))
+  {
+   store+=incoming;
+   printf("Bucket buffer size %d out of %d\n",store,buck_size);
+  }else{
+   printf("Dropped %d no of packet\n",incoming-(buck_size-store));
+   printf("Bucket buffer size %d out of %d\n",store,buck_size);
+   store=buck_size;
+  }
+  store=store-outgoing;
+  printf("After outgoing %d packets left out of %d in buffer\n",store,buck_size);
+  n--;
+ }
+}
+
